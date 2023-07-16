e7b9af295d4fe278ad309d67330b23246b663aef
Arslan Matloob <raoarslan263@gmail.com>
2023-07-16 19:19:29 +0500

    slack

--- src/views/AppLandingView.vue
+++ src/views/AppLandingView.vue
@@ -57,25 +57,25 @@
     </p>
     <div class="nav-bar-comp-logos u-text--center">
       <div class="row first-section-companies__logos">
-        <div class="col-lg-2 col-md-4 col-sm-4 col-4 logos-bar">
+        <div class="col-lg-1.5 col-md-4 col-sm-4 col-4 logos-bar">
           <img src="../images/Logos/Airbnb.png" alt="Air BnB" />
         </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-4 logos-bar">
+        <div class="col-lg-1.5 col-md-4 col-sm-4 col-4 logos-bar">
           <img src="../images/Logos/AdobePS.png" alt="Adobe PhotoShop" />
         </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-4 logos-bar">
+        <div class="col-lg-1.5 col-md-4 col-sm-4 col-4 logos-bar">
           <img src="../images/Logos/NASA.png" alt="NASA" />
         </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-4 logos-bar">
+        <div class="col-lg-1.5 col-md-4 col-sm-4 col-4 logos-bar">
           <img src="../images/Logos/etsy.png" alt="Etsy" />
         </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-4 logos-bar">
+        <div class="col-lg-1.5 col-md-4 col-sm-4 col-4 logos-bar">
           <img
             src="../images/Logos/newyorktimes.png"
             alt="The New York Times"
           />
         </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-4 logos-bar">
+        <div class="col-lg-1.5 col-md-4 col-sm-4 col-4 logos-bar">
           <img src="../images/Logos/uber.png" alt="Uber" />
         </div>
       </div>
@@ -341,6 +341,7 @@ export default {
   font-size: 0.875rem;
   margin-bottom: 1rem;
   text-transform: uppercase;
+  margin-top: 20px;
 }
 .u-text--center {
   margin-top: 5px;
@@ -391,7 +392,7 @@ p {
     justify-content: space-around;
   }
   .first-section-companies__logos .logos-bar {
-    max-width: 170px;
+    max-width: 120px;
     margin: 10px;
     text-align: center;
   }
